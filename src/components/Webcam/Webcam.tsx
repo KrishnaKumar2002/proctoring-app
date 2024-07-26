916bd597ce36d81564ac2db59decde9729d232e3
Alwyn <alwyn@interviewdesk.in>
2024-07-26 13:27:44 +0530

    Added Landmarks Model for Proctering

--- src/components/Webcam/Webcam.tsx
+++ src/components/Webcam/Webcam.tsx
@@ -1,22 +1,108 @@
-import { message } from 'antd';
-import React from 'react';
+import { useRef, useEffect } from 'react';
 import Webcam from 'react-webcam';
+import { createDetector, Face, FaceLandmarksDetector, SupportedModels } from "tensorflow-models-face-landmarks-detection"
 
 export function WebcamHolder() {
-  const videoConstraints = {
-    width: 1280,
-    height: 720,
-    facingMode: 'user',
+  const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const detect = async (net: FaceLandmarksDetector) => {
+    if (
+      webcamRef.current &&
+      webcamRef.current.video?.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = video.videoWidth;
+      const videoHeight = video.videoHeight;
+
+      video.width = videoWidth;
+      video.height = videoHeight;
+
+      if (canvasRef.current) {
+        canvasRef.current.width = videoWidth;
+        canvasRef.current.height = videoHeight;
+      }
+
+      const faces = await net.estimateFaces(video);
+      console.log(faces);
+      
+      if (canvasRef.current) {
+        const ctx = canvasRef.current.getContext('2d');
+        if (ctx) {
+          drawFaceLandmarks(ctx, faces);
+        }
+      }
+    }
   };
 
-  const onUserMediaError = (error: any) => {
-    console.error('Error: ', error);
-    message.error('unexpected Error Occured: ', error);
+  const drawFaceLandmarks = (ctx: CanvasRenderingContext2D, faces: Face[]) => {
+    faces.forEach(face => {
+      const keypoints = face.keypoints;
+      ctx.fillStyle = 'aqua';
+      for (let i = 0; i < keypoints.length; i++) {
+        const x = keypoints[i].x;
+        const y = keypoints[i].y;
+        ctx.beginPath();
+        ctx.arc(x, y, 3, 0, 2 * Math.PI);
+        ctx.fill();
+      }
+    });
   };
+
+  const runFaceMesh = async () => {
+    const model = SupportedModels.MediaPipeFaceMesh;
+    const detectorConfig = {
+      runtime: 'mediapipe' as const,
+      solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
+      refineLandmarks: true,
+      maxFaces: 1,
+    };
+    return await createDetector(model, detectorConfig);
+  };
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+    runFaceMesh().then(detector => {
+      intervalId = setInterval(() => {
+        detect(detector);
+      }, 0);
+    });
+
+    return () => {
+      if (intervalId) clearInterval(intervalId);
+    };
+  }, []);
+
   return (
-    <Webcam
-      videoConstraints={videoConstraints}
-      onUserMediaError={onUserMediaError}
-    />
+    <>
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+        </>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,7 @@
 import { Col, Row, Typography } from "antd";
 import React from "react";
 import { WebcamHolder } from "../../components/Webcam";
-import * as tf from "@tensorflow/tfjs";
-
 function Home() {
-	const tensorA = tf.tensor([
-		[1, 2],
-		[3, 4],
-	]);
-	const tensorB = tf.tensor([
-		[5, 6],
-		[7, 8],
-	]);
-	const result = tensorA.add(tensorB);
 	return (
 		<div>
 			<Row>
@@ -23,16 +12,6 @@ function Home() {
 			<Row>
 				<Col span={24} style={{ display: "flex", justifyContent: "center" }}>
 					<WebcamHolder />
-				</Col>
-			</Row>
-			<Row>
-				<Col span={24} style={{ display: "flex", justifyContent: "center" }}>
-					<Row>
-						<Col span={12} style={{ textAlign: "center" }}>
-							<Typography.Title level={1}>Tensorflow.js</Typography.Title>
-							<Typography.Text>Result : {result.toString()}</Typography.Text>
-						</Col>
-					</Row>
 				</Col>
 			</Row>
 		</div>
